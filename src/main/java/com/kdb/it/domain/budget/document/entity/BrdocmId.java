38a6733c3d9f77c6b857c011563c970084b5584c
gonnabe88 <gonnabe88@gmail.com>
2026-04-22 19:48:35 +0900

    feat: BrdocmId 복합키 클래스 추가

---
+++ src/main/java/com/kdb/it/domain/budget/document/entity/BrdocmId.java
@@ -0,0 +1,34 @@
+package com.kdb.it.domain.budget.document.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * 요구사항 정의서(Brdocm) 엔티티의 복합 기본키 클래스
+ *
+ * <p>JPA의 {@code @IdClass} 방식으로 {@link Brdocm} 엔티티의 복합키를 정의합니다.
+ * {@link Brdocm}의 {@code @Id} 필드({@code docMngNo}, {@code docVrs})와
+ * 동일한 이름과 타입을 가져야 합니다.</p>
+ *
+ * <p>JPA 복합키 클래스 요구사항:</p>
+ * <ul>
+ *   <li>{@link Serializable} 구현 필수</li>
+ *   <li>기본 생성자 필수</li>
+ *   <li>{@code equals()}, {@code hashCode()} 재정의 필수</li>
+ * </ul>
+ */
+@NoArgsConstructor  // JPA 요구사항: 기본 생성자 필수
+@AllArgsConstructor // 모든 필드를 받는 생성자 (직접 생성용)
+@EqualsAndHashCode  // equals(), hashCode() 자동 생성 (JPA 1차 캐시 동등성 비교에 필수)
+public class BrdocmId implements Serializable {
+
+    /** 문서관리번호: Brdocm.docMngNo와 이름/타입 일치 필수 */
+    private String docMngNo;
+
+    /** 문서버전: Brdocm.docVrs와 이름/타입 일치 필수 (Oracle NUMBER(4,2)) */
+    private BigDecimal docVrs;
+}
